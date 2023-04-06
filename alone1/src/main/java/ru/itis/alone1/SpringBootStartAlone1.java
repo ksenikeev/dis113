f6701210efbee8dc123fecf999bb0e5362829fe6
ksenikeev <ksenikeev@mail.ru>
2023-04-06 10:45:20 +0300

    start class

---
+++ alone1/src/main/java/ru/itis/alone1/SpringBootStartAlone1.java
@@ -0,0 +1,11 @@
+package ru.itis.alone1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartAlone1 {
+    public static void main(String[] args) {
+                SpringApplication.run(SpringBootStartAlone1.class, args);
+    }
+}
